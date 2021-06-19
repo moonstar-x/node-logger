981b8df37e74f9fd44d9d40d609443a767e5ea8a
moonstar-x <christian.lopez99@outlook.com>
2021-06-18 23:28:28 -0500

    Added typings.

--- src/index.js
+++ src/index.js
@@ -15,30 +15,64 @@ const prepareMessage = (chalkStyle, prefix, args) => {
   return chalkStyle(`(${new Date().toLocaleTimeString()}) - ${chalk.bold(prefix)} - `, ...parseArguments(Array.prototype.slice.call(args)));
 };
 
+/**
+ * Log a LOG level message in white.
+ * @param  {...any} arguments 
+ * @returns {undefined}   
+ */
 const log = (...arguments) => {
   console.log(prepareMessage(chalk.white, '[LOG]', arguments));
 };
 
+/**
+ * Log an INFO level message in cyan.
+ * @param  {...any} arguments 
+ * @returns {undefined}   
+ */
 const info = (...arguments) => {
   console.info(prepareMessage(chalk.cyan, '[INFO]', arguments));
 };
 
+/**
+ * Log an ERROR level message in red.
+ * @param  {...any} arguments 
+ * @returns {undefined}   
+ */
 const error = (...arguments) => {
   console.error(prepareMessage(chalk.red, '[ERROR]', arguments));
 };
 
+/**
+ * Log a FATAL level message in red.
+ * @param  {...any} arguments 
+ * @returns {undefined}   
+ */
 const fatal = (...arguments) => {
   console.error(prepareMessage(chalk.red, '[FATAL]', arguments));
 };
 
+/**
+ * Log a WARN level message in yellow.
+ * @param  {...any} arguments 
+ * @returns {undefined}   
+ */
 const warn = (...arguments) => {
   console.warn(prepareMessage(chalk.yellow, '[WARN]', arguments));
 };
 
+/**
+ * Log a DEBUG level message in green.
+ * @param  {...any} arguments 
+ * @returns {undefined}   
+ */
 const debug = (...arguments) => {
   console.debug(prepareMessage(chalk.green, '[DEBUG]', arguments));
 };
 
+/**
+ * Clears the console.
+ * @returns {undefined}
+ */
 const clear = () => {
   console.clear();
 };
